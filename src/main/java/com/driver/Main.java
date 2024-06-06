e4429e2c0bd146c4da611a89228f97f01e682432
Mohd Arif <arifm0949@gmail.com>
2024-06-06 20:16:12 +0530

    Add getter and setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Arif"; name variable is private so write getter and setter
+//        System.out.println(obj.name);  can't print as name is private
+
+        obj.setName("Arif");
+        System.out.println(obj.getName());
+    }
 }
