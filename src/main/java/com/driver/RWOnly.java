d61ff3755d587e94a177951dfe5972b422216f47
Mohd Arif <arifm0949@gmail.com>
2024-07-06 17:22:41 +0530

    make function public

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,11 +3,11 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    void setName(String name){
+    public void setName(String name){
        this.name = name;
     }
 
-    String getName(){
+   public String getName(){
         return this.name;
     }
     
